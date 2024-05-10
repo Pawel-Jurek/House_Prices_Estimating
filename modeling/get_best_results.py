18d9bef1adcc0b17c3d89b6897507b3880fac5b5
Paweł Jurek <pawel80121@gmail.com>
2024-05-10 10:35:09 +0200

    testing 9-10 weights and get best results from current tests

---
+++ modeling/get_best_results.py
@@ -0,0 +1,24 @@
+try:
+    with open("clean_results.txt", "r", encoding="utf-8") as file:
+        lines = file.readlines()
+
+    results = []
+    weights = []
+    values = {}
+
+    if lines:
+        for line in lines:
+            data = line.strip().split(": ")
+            results.append(float(data[1]))
+            weights.append(data[0])
+        results.sort(reverse=True)
+        for res in results[:10]:
+            values[weights[results.index(res)]] = res
+        
+        with open("best_results.txt", "w", encoding="utf-8") as file:
+            for key, value in values.items():
+                file.write(f"{key}: {value}\n")
+            
+    
+except FileNotFoundError:
+    print("File not found")
