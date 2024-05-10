6e93cf46375e4a8bde2be00cb10af17086eb2827
Paweł Jurek <pawel80121@gmail.com>
2024-05-10 11:08:33 +0200

    fixed best results function

--- modeling/get_best_results.py
+++ modeling/get_best_results.py
@@ -11,13 +11,16 @@ try:
             data = line.strip().split(": ")
             results.append(float(data[1]))
             weights.append(data[0])
-        results.sort(reverse=True)
-        for res in results[:10]:
+        sorted_results = results.copy()
+        sorted_results.sort(reverse=True)
+        for res in sorted_results[:10]:
             values[weights[results.index(res)]] = res
+        
         
         with open("best_results.txt", "w", encoding="utf-8") as file:
             for key, value in values.items():
                 file.write(f"{key}: {value}\n")
+        
             
     
 except FileNotFoundError:
