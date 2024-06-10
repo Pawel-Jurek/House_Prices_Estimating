d95c2b79602ca4eae9c9925c0176e10a8e0c5e67
Paweł Jurek <pawel80121@gmail.com>
2024-06-10 15:16:48 +0200

    connection to azure database

--- apartament_valuation/apartament_valuation/settings.py
+++ apartament_valuation/apartament_valuation/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,12 +83,20 @@ WSGI_APPLICATION = 'apartament_valuation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "houses-db",
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": "apartament-valuation.postgres.database.azure.com",
+        "PORT": "5432",
+        "OPTIONS": {"sslmode": "require"},
     }
 }
+
 
 
 # Password validation
