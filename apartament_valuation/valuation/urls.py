f4e250fd91b97fa3d5c0f6b860071f272f4d85d8
Paweł Jurek <pawel80121@gmail.com>
2024-10-30 23:51:42 +0100

    Quartal to month and homepage endpoint

--- apartament_valuation/valuation/urls.py
+++ apartament_valuation/valuation/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [ 
     path('addresses/<str:city>/', views.addresses, name = "addresses"),
+    path('get-home-data/', views.get_home_data),
     path('', views.valuation, name = "valuation"),
 ]
